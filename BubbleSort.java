daeb9d5dc35f61d038747232b46f796dfa30a356
vijaykumarmganj1 <vijaykumarmganj1@gmail.com>
2025-06-29 11:10:05 +0530

    some changes in file of java folder

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,18 +1,22 @@
 public class BubbleSort {
-    public static void main(String1 args[]){        int arr[] ={7,8,3,1,2};
-        int n=arr.length;
-        // bubble short 
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<n-1-i;j++){
-                if(arr[j] > arr[j+1]){
-                    int temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+    public static void main(String[] args) {
+        int arr[] = { 7, 8, 3, 1, 2 };
+        int n = arr.length;
+        // bubble short
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            System.out.print(arr[i]);
+
+        for(int a : arr) {
+            System.out.print(a + " ");
         }
     }
 }
+
+
